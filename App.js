802dcdaaeac6afae923aa253a3dd096948d7616a
JoseF <jose.tf2396@gmail.com>
2022-04-04 19:34:59 -0700

    React Context API

--- App.js
+++ App.js
@@ -1,2 +1,19 @@
-//test
-//test 2
+import React from 'react';
+const ThemeContext = React.createContext(
+  /* optional default value */
+);
+const App = props => (
+  <ThemeContext.Provider value={{ primaryColor: green }}>
+    {props.children}
+  </ThemeContext.Provider>
+);
+
+const ThemedButton = () => (
+  <ThemeContext.Consumer>
+    {value => (
+      <Button primaryColor={{ value.primaryColor }}>
+        I'm button using context!
+      </Button>
+    )}
+  </ThemeContext.Consumer>
+);
